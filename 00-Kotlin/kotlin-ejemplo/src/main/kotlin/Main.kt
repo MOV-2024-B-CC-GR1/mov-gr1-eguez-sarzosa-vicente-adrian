3451c58813c57b24955f81a40949d437a27b54dd
Adrian Eguez <eadepto@hotmail.com>
2024-10-24 10:37:02 -0500

    kotlin 1

--- 00-Kotlin/kotlin-ejemplo/src/main/kotlin/Main.kt
+++ 00-Kotlin/kotlin-ejemplo/src/main/kotlin/Main.kt
@@ -1,7 +1,58 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.util.*
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+
+    // INMUTABLES (No se RE ASIGNA "=")
+    val inmutable: String = "Adrian";
+    // inmutable = "Vicente" // Error!
+    // MUTABLES
+    var mutable: String = "Vicente"
+    mutable = "Adrian" // Ok
+    // VAL > VAR
+
+
+    // Duck Typing
+    val ejemploVariable = " Adrian Eguez "
+    ejemploVariable.trim()
+    val edadEjemplo: Int = 12
+    // ejemploVariable = edadEjemplo // Error!
+    // Variables Primitivas
+    val nombreProfesor: String = "Adrian Eguez"
+    val sueldo: Double = 1.2
+    val estadoCivil: Char = 'C'
+    val mayorEdad:Boolean = true
+    // Clases en Java
+    val fechaNacimiento: Date = Date()
+
+    // When (Switch)
+    val estadoCivilWhen = "C"
+    when (estadoCivilWhen){
+        ("C") ->{
+            println("Casado")
+        }
+        "S" -> {
+            println("Soltero")
+        }
+        else ->{
+            println("No sabemos")
+        }
+    }
+    val esSoltero = (estadoCivilWhen == "S")
+    val coqueteo = if (esSoltero) "Si" else "No" // if else chiquito
+
+    imprimirNombre("Adrian")
+}
+
+
+fun imprimirNombre(nombre:String): Unit{
+    fun otraFuncionAdentro(){
+        println("Otra funcion adentro")
+    }
+    println("Nombre: $nombre") // Uso sin llaves
+    println("Nombre: ${nombre}") // Uso con llaves opcional
+    println("Nombre: ${nombre + nombre}") // Uso con llaves (concatenado)
+    println("Nombre: ${nombre.toString()}") // uso con llaves (funcion)
+    println("Nombre: $nombre.toString()") // INCORRECTO!
+    // (no pueden usar sin llaves)
+    otraFuncionAdentro()
 }
